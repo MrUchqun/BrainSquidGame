65885fba22c1e92724ac6bc9397fb3409a13f77d
diyorbek57 <58900685+diyorbek57@users.noreply.github.com>
2021-12-06 18:41:59 +0500

    Initial commit

---
+++ app/src/main/java/com/b12/game/MainActivity.java
@@ -0,0 +1,17 @@
+package com.b12.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+
+    }
+}
