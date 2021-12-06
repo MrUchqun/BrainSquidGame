e1b0af3eb35063c905417071d22454d084ca4b75
azizjon7205 <azizjonsheronov2607@gmail.com>
2021-12-06 22:44:45 +0500

    I have added SplashActivity: Azizjon

---
+++ app/src/main/java/com/b12/game/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.b12.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread startTimer = new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent0 = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent0);
+                    //overridePendingTransition(R.anim.fadin, R.anim.fadout);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        startTimer.start();
+    }
+}
