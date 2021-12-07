4efc04d0172ee71d11d85a679af0c51f09c41878
azizjon7205 <azizjonsheronov2607@gmail.com>
2021-12-07 18:44:16 +0500

    I have add changes

--- app/src/main/java/com/b12/game/MainActivity.java
+++ app/src/main/java/com/b12/game/MainActivity.java
@@ -1,5 +1,6 @@
 package com.b12.game;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,9 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SplashActivity splashActivity = new SplashActivity();
-        splashActivity.changeStatusBarColor(this);
+//        SplashActivity splashActivity = new SplashActivity();
+//        splashActivity.changeStatusBarColor(this);
 
+        CardView cardView = findViewById(R.id.play_game_1);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity1.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
