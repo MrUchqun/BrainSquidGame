11607069409872fc1dc0f4aec633d0362374d767
azizjon7205 <azizjonsheronov2607@gmail.com>
2021-12-13 00:16:45 +0500

    I have added changes

---
+++ app/src/main/java/com/b12/game/activities/SecondGame3x3Activity.kt
@@ -0,0 +1,12 @@
+package com.b12.game.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.b12.game.R
+
+class SecondGame3x3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_game3x3)
+    }
+}
