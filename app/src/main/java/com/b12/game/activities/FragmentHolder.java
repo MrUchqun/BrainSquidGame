04bb269f4a44703b4a4d1dcfb166ba01bc1989e1
Khamdamov <58900685+diyorbek57@users.noreply.github.com>
2021-12-12 22:08:10 +0500

    1) CountDownTimer implementation 2) added fragment assigment and answer 3) JDK changed from 1.8 to 11 4) added maven jitpack

---
+++ app/src/main/java/com/b12/game/activities/FragmentHolder.java
@@ -0,0 +1,35 @@
+package com.b12.game.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.b12.game.R;
+import com.b12.game.fragments.FragmentFirstGameAssignment;
+
+public class FragmentHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_holder);
+        SplashActivity splashActivity = new SplashActivity();
+        splashActivity.changeStatusBarColor(this);
+        Fragment someFragment = new FragmentFirstGameAssignment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_holder, someFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent = new Intent(FragmentHolder.this, GameActivity1.class);
+        startActivity(intent);
+        finish();
+    }
+}
